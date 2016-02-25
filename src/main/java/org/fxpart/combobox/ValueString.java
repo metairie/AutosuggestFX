b01c33985b190f7e2f32690152bc9971f10d9b83
Moutenet <sopra_om@ebu.ch>
2016-02-25 17:55:39 +0100

    ITAPPSNEOS-1693 - preparation migration refactoring

---
+++ src/main/java/org/fxpart/combobox/ValueString.java
@@ -0,0 +1,10 @@
+package org.fxpart.combobox;
+
+import org.fxpart.common.bean.KeyValue;
+
+/**
+ * Created by sopra_om on 25.02.2016.
+ */
+public abstract class ValueString<T> extends KeyValue<T,String> {
+
+}
